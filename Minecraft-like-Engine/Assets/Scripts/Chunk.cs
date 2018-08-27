dfba818178166498a7f16e1ead3e9dc4d0f618d6
ctrain <clement.train@gmail.com>
2018-08-27 23:37:53 +0200

    rename MinecraftEngine and create Real TheCore unity code

---
+++ Minecraft-like-Engine/Assets/Scripts/Chunk.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; // For List class;
+
+public class Chunk
+{
+
+    // Size of the Chunk
+    public int sizeX = 50;
+    public int sizeY = 30;
+    public int sizeZ = 50;
+
+    // Declare the 3D block Array
+    public int[,,] blocksArray;
+
+
+    // Initialize the block array
+    public void Initialize()
+    {
+        blocksArray = new int[sizeX, sizeY, sizeZ];
+    }
+}
